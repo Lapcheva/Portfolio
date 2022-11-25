3cebb5c8490677045286949991b56f9a7217ef22
nadine <nadine@nadines-MacBook-Air.local>
2022-11-25 23:34:49 +0200

    finished desktop version

---
+++ Coffee_Roasters/JS/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.testimonials-carousel').slick(
+        {
+            dots: true,
+            arrows: false,
+            infinite: true,
+            speed: 300,
+            slidesToShow: 1,
+            adaptiveHeight: true
+        }
+
+    );
+});
